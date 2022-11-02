4e5d66316d3f5a000fd219d080288a4b79b3e160
Jonathan Brown <jbrown@acuity.social>
2022-11-02 21:43:36 +0700

    Update for latest Substrate.

--- src/keys.ts
+++ src/keys.ts
@@ -6,11 +6,11 @@ let keys = JSONbig.parse(fs.readFileSync('keys.json', 'utf8'));
 
 for (let node of keys) {
   console.log("(");
-  console.log("hex![\"" + node[0].publicKey.slice(2) + "\"].into(),");
-  console.log("hex![\"" + node[1].publicKey.slice(2) + "\"].into(),");
-  console.log("hex![\"" + node[2].publicKey.slice(2) + "\"].unchecked_into(),");
-  console.log("hex![\"" + node[3].publicKey.slice(2) + "\"].unchecked_into(),");
-  console.log("hex![\"" + node[3].publicKey.slice(2) + "\"].unchecked_into(),");
-  console.log("hex![\"" + node[3].publicKey.slice(2) + "\"].unchecked_into(),");
+  console.log("array_bytes::hex_n_into_unchecked(\"" + node[0].publicKey.slice(2) + "\"),");
+  console.log("array_bytes::hex_n_into_unchecked(\"" + node[1].publicKey.slice(2) + "\"),");
+  console.log("array_bytes::hex2array_unchecked(\"" + node[2].publicKey.slice(2) + "\").unchecked_into(),");
+  console.log("array_bytes::hex2array_unchecked(\"" + node[3].publicKey.slice(2) + "\").unchecked_into(),");
+  console.log("array_bytes::hex2array_unchecked(\"" + node[3].publicKey.slice(2) + "\").unchecked_into(),");
+  console.log("array_bytes::hex2array_unchecked(\"" + node[3].publicKey.slice(2) + "\").unchecked_into(),");
   console.log("),");
 }
